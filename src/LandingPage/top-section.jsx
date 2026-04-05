4b8f6830f2e0ea950f7ae655163442de587f3b8c
Amit Sikdar <sikdara477@gmail.com>
2026-04-05 20:49:08 +0530

    added nav-bar animation

--- src/LandingPage/top-section.jsx
+++ src/LandingPage/top-section.jsx
@@ -1,9 +1,37 @@
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import logo from "./assets/icons/simple-icons_codecrafters.svg"
 import vector from "./assets/icons/Vector.svg"
 import "./style/top-section.css"
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 export function Navbar() {
+  const navbar = useRef()
+
+  const { contextSafe } = useGSAP({ scope: navbar })
+
+  const onEnter = contextSafe((e) => {
+    const content = e.currentTarget.querySelector('.nav-btn-content')
+    gsap.to(content, {
+      y: -29,
+      duration: 0.5,
+      ease: "power2.inOut"
+    })
+  })
+
+  const onLeave = contextSafe((e) => {
+    const content = e.currentTarget.querySelector('.nav-btn-content')
+    gsap.to(content, {
+      y: 0,
+      duration: 0.5,
+      ease: "power2.inOut"
+    })
+  })
+
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
@@ -17,29 +45,51 @@ export function Navbar() {
         <img src={logo} alt="logo" />
         <p>Amit.</p>
       </div>
-      <div className="navbar">
+      <div className="navbar" ref={navbar}>
         <div className="nav-item">
-          <button onClick={() => scrollToSection('about')}>
-            About Me
+          <button 
+          onClick={() => scrollToSection('about')}
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}>
+            <span className="nav-btn-content">
+              <span>About Me</span>
+              <span>About Me</span>
+            </span>
           </button>
         </div>
         <div className="nav-item">
           <Link to="/projects">
-            <button>
-              Projects
+            <button 
+            onMouseEnter={onEnter}
+            onMouseLeave={onLeave}>
+              <span className="nav-btn-content">
+                <span>Projects</span>
+                <span>Projects</span>
+              </span>
             </button>
           </Link>
         </div>
         <div className= "nav-item">
           <Link to="/ai-bot">
-            <button>
-              AI Bot
+            <button 
+            onMouseEnter={onEnter}
+            onMouseLeave={onLeave}>
+              <span className="nav-btn-content">
+                <span>AI Bot</span>
+                <span>AI Bot</span>
+              </span>
             </button>
           </Link>
         </div>
         <div className="nav-item">
-          <button onClick={() => scrollToSection('contact')}>
-            Socials
+          <button 
+          onClick={() => scrollToSection('contact')}
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}>
+            <span className="nav-btn-content">
+              <span>Socials</span>
+              <span>Socials</span>
+            </span>
           </button>
         </div>
       </div>
@@ -51,4 +101,4 @@ export function Navbar() {
       </div>
     </div>
   )
-}
+}
