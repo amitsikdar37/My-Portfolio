ac4ee9bcf294860c64f91aebfc9503f599e18448
Amit Sikdar <sikdara477@gmail.com>
2026-04-11 15:28:12 +0530

    added animation to landing page

--- src/LandingPage/lp.jsx
+++ src/LandingPage/lp.jsx
@@ -2,7 +2,45 @@ import { Navbar } from "./top-section"
 import "./style/lp.css"
 import { HeroSection } from "./hero-section"
 
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(useGSAP)
+
+
 export function LandingPage() {
+  useGSAP(() => {
+    gsap.from('.navbar', {
+      opacity: 0,
+      y: -100,
+      scale: 1.5,
+      duration: 1,
+      ease: 'power2.out'
+    })
+
+    gsap.from('.right-side', {
+      opacity: 0,
+      x: 100,
+      duration: 1,
+      ease: 'power2.out'
+    })
+
+    gsap.from('.hero-section', {
+      opacity: 0,
+      y: 100,
+      scale: 1.5,
+      duration: 1,
+      ease: 'power2.out'
+    })
+
+    gsap.from('.left-side', {
+      opacity: 0,
+      x: -100,
+      duration: 1,
+      ease: 'power2.out'
+    })
+  })
+
   return (
     <div className="landing-page">
       <Navbar />
